fdd4f244e82fc98aabc1f4efbc1f51f69aa0d8d9
KalpesH Patil <87258301+KalpesH33@users.noreply.github.com>
2022-08-21 17:42:01 +0530

    Update GuessingGame.cs

--- Games/GuessingGame.cs
+++ Games/GuessingGame.cs
@@ -1,4 +1,3 @@
-// Online C# Editor for free
 //Guessing Game
 
 using System;
@@ -7,31 +6,41 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-     string MasterCode = "";
-     
-     Console.WriteLine("Please set the Master Code : ");
-     MasterCode = Console.ReadLine();
-    
-     string GuessWord = "";
-     int ChanceCount = 0;
-     int Chances = 3;
-     while (GuessWord != MasterCode )
-     {
-         if (ChanceCount < Chances )
-         {  
-             Console.WriteLine("Guess the word ");
-             GuessWord = Console.ReadLine();
-             ChanceCount++;
-             
-         }
-         else 
-         {
-             Console.WriteLine("You lost");
-             
-         }
-     }
- 
-     Console.WriteLine("You Won");
- 
+        string MasterCode = "";
+
+        Console.WriteLine("Please set the Master Code : ");
+        MasterCode = Console.ReadLine();
+        Console.WriteLine("3 Chances Left");
+
+        string GuessWord = "";
+        int Count = 0;
+        int Chances = 3;
+        bool LimitOver = false;
+        while (GuessWord != MasterCode  && !LimitOver)
+        {
+            if (Count < Chances)
+            {
+                Console.WriteLine("Guess the word ");
+                GuessWord = Console.ReadLine();
+                Count++;
+
+            }
+            else
+            {
+                LimitOver = true;
+
+            }
+        }
+        if (LimitOver)
+        {
+            Console.WriteLine("You Loose");
+        }
+        else
+        {
+            Console.WriteLine("You Won");
+
+        }
+       
+
     }
 }
