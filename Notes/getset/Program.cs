00384a18aaa25b7b83391adc957af747bf8724ec
KalpesH Patil <87258301+KalpesH33@users.noreply.github.com>
2022-08-26 14:16:15 +0530

    Update Program.cs

--- Notes/getset/Program.cs
+++ Notes/getset/Program.cs
@@ -4,6 +4,9 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
+        
+        StudentData Student1 = new studentData ("Kalpesh", 21,"A" )
+            Console.WriteLine(Student1.StudentDivision);
       
     }
 }
