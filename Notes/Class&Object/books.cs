65c29ebdf2953caa177e743ec2e4e6b555f992d7
KalpesH Patil <87258301+KalpesH33@users.noreply.github.com>
2022-08-26 13:29:27 +0530

    Update books.cs

--- Notes/Class&Object/books.cs
+++ Notes/Class&Object/books.cs
@@ -3,6 +3,18 @@ using System;
 namespace book;
 class books
 {
+  public string EmpName;
+  public int EmpID;
+  public string Address;
+  
+  public book(String aEmpName, String aEmpID, String aAddress )// Constructor 
+   {
+        EmpName = aEmpName;
+        EmpID = aEmpID;
+        Address = aAddress;
+  
+  }
+  
   
 
 }
