c48da8e844e568f02b96b98d3355780ab585e8b3
KalpesH Patil <87258301+KalpesH33@users.noreply.github.com>
2022-08-26 13:38:57 +0530

    Update Program.cs

--- Notes/Class&Object/Program.cs
+++ Notes/Class&Object/Program.cs
@@ -7,10 +7,14 @@ class program
   
   static void Main(String[] args)
   {
-    
-    
+     book book1 = new book("John",2021,"USA") // Creating object of book class construnctor;
+       
+       book1.EmpID = 2303; // changing valu; 
+       
+     Console.WriteLine(book1.EmpID); // printing the value;
+        
   }
-  
+    
 
 
 }
